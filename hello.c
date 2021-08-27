d419bed9d242bef7c3b9ed31d385b62f7996952b
r99712 <rspayth2@huskers.unl.edu>
2021-08-27 01:04:27 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/**
+ * Author: Robert Spayth
+ * Date: 2021/08/23
+ */ 
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    printf("Robby Spayth\n");
+    return 0;
+}
