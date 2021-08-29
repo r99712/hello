720360def590b7525c359209f72ac542dff5ba3d
Mark Mahdi <mmahdi2@huskers.unl.edu>
2021-08-29 03:25:31 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,12 +1,12 @@
 /**
  * Author: Robert Spayth
  * Date: 2021/08/23
- */ 
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv){
-    printf("Robby Spayth\nElectrical Engineering\n");
+    printf("Robby Spayth\nElectrical Engineering\nMark Mahdi\nComputer Science\n");
     return 0;
 }
